5ece79c6c79e6669d58b92d683790a328440f1bb
Alexandr <megalistnn@gmail.com>
2023-08-12 07:44:09 +0300

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let str = '' + n;
+	const arr = str.split('');
+	const filteredArr = arr.filter((elem) => elem !== '-');
+	const reversedStr = filteredArr.reverse().join('');
+	const reversedNumber = parseInt(reversedStr, 10);
+	return reversedNumber;
 }
